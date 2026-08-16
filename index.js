9fbaa4002bba7f398cf457e0787fab58c8ee671a
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2026-08-16 19:38:14 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+const PORT =  process.env.PORT
+
+const app = express()
+app.use(express.json())
+
+
+
+app.get("/",async(req,res)=>{
+    res.status(200).json({message:"hello from aws"})
+})
+
+
+app.listen(PORT,()=>{
+    console.log("Server started on " + PORT)
+})
